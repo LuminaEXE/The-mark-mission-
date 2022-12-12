d098db3c7326bb94d21477c3950912ff0a44df9c
LuminaEXE <86614978+LuminaEXE@users.noreply.github.com>
2022-12-12 17:52:42 -0600

    s
    s

---
+++ root/src/JS/index.js
@@ -0,0 +1,12 @@
+
+
+let event = document.getElementById("women");
+let id = document.getElementById("scD")
+
+
+event.addEventListener(
+	'click', () => {
+		id.scrollIntoView();
+		console.log("mmen")
+	}
+)
