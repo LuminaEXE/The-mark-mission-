6b55b8144f58e8f3edd78860611872798e6c1ee9
LuminaEXE <86614978+LuminaEXE@users.noreply.github.com>
2022-12-12 19:07:31 -0600

    Revert "asd"
    This reverts commit 8db5a544302752a3943139eb71422fe46785802f.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js,tsx,ts,jsx}"],
+  content: ["./root/src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
